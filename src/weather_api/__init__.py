3173ee76c86bd41d0b9d6cb91f27e9e4d2ced319
Ralph McHugh <mchuralp@isu.edu>
2024-03-04 14:20:55 -0700

    Basic Quart setup from Prof's example

---
+++ src/weather_api/__init__.py
@@ -0,0 +1,31 @@
+from quart import Quart, jsonify, request
+
+app = Quart(__name__)
+
+@app.get("/example")
+async def example():
+    return jsonify(["a", "b"])
+
+
+@app.post("/echo")
+async def echo():
+    data = await request.get_json()
+    return {"input": data, "extra": True}
+
+def run() -> None:
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
