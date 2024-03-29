c3bb8444e7d894bb8d222d4196b3a99597831606
Jane Lee <109120467+jomuni2@users.noreply.github.com>
2024-03-29 13:32:16 -0600

    Feature/weather sensor array (#12)
    * setup the sensors skeleton and filled it out with all the functions related to my AQI part
    
    * added the aqi module for later and blackened everything manually
    
    * Added fake sensors for wind direction/speed, as well as read/clean functions for those values
    
    * added fake sensors for precipitation
    
    * Added test files for the sensor folder
    
    * manually blackened files
    
    ---------
    
    Co-authored-by: Ralph McHugh <mchuralp@isu.edu>
    Co-authored-by: williamforde <williamforde@isu.edu>
    Co-authored-by: Jane Lee <jeongeunlee@isu.edu>

---
+++ src/sensors/sensorFakes.py
@@ -0,0 +1,61 @@
+# This module is used to generate fake sensor data so that if real sensors were not available, we could still test the system.
+import random as rand
+
+# This zipcode will eventually have a post function to change it
+zipCode = 83201  # Pocatello, ID
+rand.seed(zipCode)
+
+
+# Real pollutant values are not necessarily bounded, but we will assume that the values are between 0 and 300
+# A triangular distibution just makes the numbers look more realistic
+def sensePMTwoPointFive():
+    dataContaminationAmount = rand.random()
+    pmTwoPointFive = rand.triangular(0, 300, 10) + dataContaminationAmount
+    return pmTwoPointFive
+
+
+def sensePMTen():
+    dataContaminationAmount = rand.random()
+    pmTen = rand.triangular(0, 300, 10) + dataContaminationAmount
+    return pmTen
+
+
+def senseSulfurDioxide():
+    dataContaminationAmount = rand.random()
+    sulfurDioxide = rand.triangular(0, 300, 10) + dataContaminationAmount
+    return sulfurDioxide
+
+
+def senseNitrogenDioxide():
+    dataContaminationAmount = rand.random()
+    senseNitrogenDioxide = rand.triangular(0, 300, 10) + dataContaminationAmount
+    return senseNitrogenDioxide
+
+
+def senseOzone():
+    dataContaminationAmount = rand.random()
+    ozone = rand.triangular(0, 300, 10) + dataContaminationAmount
+    return ozone
+
+
+# Wind data
+def senseWindDirection():
+    windDirection = rand.randint(0, 360)
+    return windDirection
+
+
+def senseWindSpeed():
+    windSpeed = rand.triangular(0, 20, 5)
+    return windSpeed
+
+
+# precipitation
+def sensePrecipitationType():
+    precipitation_types = ["rain", "snow"]
+    precipitation_type = rand.choice(precipitation_types)
+    return precipitation_type
+
+
+def sensePrecipitationAmount():
+    precipitation = rand.triangular(0, 10, 0)
+    return precipitation
--- src/weather_api/__init__.py
+++ src/weather_api/__init__.py
@@ -2,33 +2,22 @@ from quart import Quart, jsonify, request
 
 app = Quart(__name__)
 
+
 @app.get("/example")
 async def example():
     return jsonify(["a", "b"])
 
+
 @app.get("/precipitation")
 async def example():
     return jsonify(["a", "b"])
+
 
 @app.post("/echo")
 async def echo():
     data = await request.get_json()
     return {"input": data, "extra": True}
 
+
 def run() -> None:
     app.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
