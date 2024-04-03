dd6a8eb4ce82deb378b5293c9bbab1fb189b9e2d
williamforde <williamforde@isu.edu>
2024-04-03 15:49:55 -0600

    Created/finished the wind builder.

--- src/weather_api/wind.py
+++ src/weather_api/wind.py
@@ -1,2 +1,20 @@
 # this is the module for the wind related weather functions
 # it should only be called by the forecast.py module
+
+# had to add the sensors directory to the path so that the readSensors module could be imported
+import sys
+sys.path.append('../sensors')
+import readSensors as sensors
+
+windArray = [[None, None] for i in range(24)]
+
+def fillWindArray():
+    for i in range(len(windArray)):
+        windArray[i][0] = sensors.readWindDirection()
+        windArray[i][1] = sensors.readWindSpeed()
+
+def setWindArray():
+    fillWindArray()
+
+def getWindArray():
+    return windArray
