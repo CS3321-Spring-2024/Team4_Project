a800098d8f5c52bf8ac30a67779adb5aef8ad7d3
ralphmchugh <98359307+ralphmchugh@users.noreply.github.com>
2024-04-09 12:40:15 -0600

    Feature/aqi (#15)
    * made some changes to the sensor files that I had missed before and finished the aqi builder
    
    * Created/finished the wind builder.
    
    * Finished aqi feature
    
    * blackened wind.py
    
    ---------
    
    Co-authored-by: williamforde <williamforde@isu.edu>

--- src/weather_api/wind.py
+++ src/weather_api/wind.py
@@ -3,18 +3,23 @@
 
 # had to add the sensors directory to the path so that the readSensors module could be imported
 import sys
-sys.path.append('../sensors')
+
+sys.path.append("../sensors")
 import readSensors as sensors
 
 windArray = [[None, None] for i in range(24)]
+
 
 def fillWindArray():
     for i in range(len(windArray)):
         windArray[i][0] = sensors.readWindDirection()
         windArray[i][1] = sensors.readWindSpeed()
 
+
 def setWindArray():
     fillWindArray()
 
+
 def getWindArray():
     return windArray
+
