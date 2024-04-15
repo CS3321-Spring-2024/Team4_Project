708e251c0db91eb8afeb15e711c522260abcc6c4
williamforde <williamforde@isu.edu>
2024-04-14 20:43:59 -0500

    Fixed issue in wind module and added unit testing

--- src/weather_api/wind.py
+++ src/weather_api/wind.py
@@ -1,23 +1,24 @@
 # this is the module for the wind related weather functions
 # it should only be called by the forecast.py module
 
-# had to add the sensors directory to the path so that the readSensors module could be imported
-
-
 from src.sensors import readSensors as sensors
 
 windArray = [[None, None] for i in range(24)]
-
+isArrayFilled = False
 
 def fillWindArray():
     for i in range(len(windArray)):
         windArray[i][0] = sensors.readWindDirection()
         windArray[i][1] = sensors.readWindSpeed()
 
-
-def setWindArray():
-    fillWindArray()
-
+def validateWindArray(arrayFilled):
+    if not arrayFilled:
+        fillWindArray()
+        arrayFilled = True
+    else:
+        arrayFilled = True
+    return arrayFilled
 
 def getWindArray():
+    validateWindArray(isArrayFilled)
     return windArray
